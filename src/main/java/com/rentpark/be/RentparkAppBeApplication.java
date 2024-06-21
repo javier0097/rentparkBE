88e7381c6f60e16f94ddbdbc99134bfd23ae180d
Javier Martinez <javier0097@hotmail.com>
2024-06-21 11:47:19 -0400

    project created

---
+++ src/main/java/com/rentpark/be/RentparkAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.rentpark.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentparkAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentparkAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rentpark/be/RentparkAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentpark.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentparkAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
